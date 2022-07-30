249592061bce979786c68fe7824c903f2ed5e28c
leixiaofeng-astar <61817895+leixiaofeng-astar@users.noreply.github.com>
2022-07-30 22:30:14 +0800

    change co-atten option for AGE to match to train model

--- models/fovea_net.py
+++ models/fovea_net.py
@@ -6,16 +6,12 @@ from __future__ import print_function
 import os
 import logging
 
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
 import torchvision.models as models
 
 import cv2
-import copy
 import numpy as np
 
-from core.inference import get_max_preds, get_heatmap_center_preds
+from core.inference import get_max_preds
 from utils.transforms import crop_and_resize
 from models.polyformer import PolyformerLayer
 from models.unet_parts import *
@@ -684,82 +680,6 @@ def get_all_indices(shape):
     return torch.stack(tuple(reversed(out)), len(shape))
 
 
-class CoattentionModel(nn.Module):
-    def __init__(self, block, layers, num_classes, all_channel=256, all_dim=60 * 60):  # 473./8=60
-        super(CoattentionModel, self).__init__()
-        self.encoder = ResNet(block, layers, num_classes)
-        self.linear_e = nn.Linear(all_channel, all_channel, bias=False)
-        self.channel = all_channel
-        self.dim = all_dim
-        self.gate = nn.Conv2d(all_channel, 1, kernel_size=1, bias=False)
-        self.gate_s = nn.Sigmoid()
-        self.conv1 = nn.Conv2d(all_channel * 2, all_channel, kernel_size=3, padding=1, bias=False)
-        self.conv2 = nn.Conv2d(all_channel * 2, all_channel, kernel_size=3, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(all_channel)
-        self.bn2 = nn.BatchNorm2d(all_channel)
-        self.prelu = nn.ReLU(inplace=True)
-        self.main_classifier1 = nn.Conv2d(all_channel, num_classes, kernel_size=1, bias=True)
-        self.main_classifier2 = nn.Conv2d(all_channel, num_classes, kernel_size=1, bias=True)
-        self.softmax = nn.Sigmoid()
-
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                # n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, 0.01)
-                # init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-                # init.xavier_normal(m.weight.data)
-                # m.bias.data.fill_(0)
-            elif isinstance(m, nn.BatchNorm2d):
-                m.weight.data.fill_(1)
-                m.bias.data.zero_()
-
-    def forward(self, input1, input2):  # 注意input2 可以是多帧图像
-
-        # input1_att, input2_att = self.coattention(input1, input2)
-        input_size = input1.size()[2:]
-        exemplar, temp = self.encoder(input1)
-        query, temp = self.encoder(input2)
-        fea_size = query.size()[2:]
-        all_dim = fea_size[0] * fea_size[1]
-        exemplar_flat = exemplar.view(-1, query.size()[1], all_dim)  # N,C,H*W
-        query_flat = query.view(-1, query.size()[1], all_dim)
-        exemplar_t = torch.transpose(exemplar_flat, 1, 2).contiguous()  # batch size x dim x num
-        exemplar_corr = self.linear_e(exemplar_t)  #
-        A = torch.bmm(exemplar_corr, query_flat)
-        A1 = F.softmax(A.clone(), dim=1)  #
-        B = F.softmax(torch.transpose(A, 1, 2), dim=1)
-        query_att = torch.bmm(exemplar_flat, A1).contiguous()  # 注意我们这个地方要不要用交互以及Residual的结构
-        exemplar_att = torch.bmm(query_flat, B).contiguous()
-
-        input1_att = exemplar_att.view(-1, query.size()[1], fea_size[0], fea_size[1])
-        input2_att = query_att.view(-1, query.size()[1], fea_size[0], fea_size[1])
-        input1_mask = self.gate(input1_att)
-        input2_mask = self.gate(input2_att)
-        input1_mask = self.gate_s(input1_mask)
-        input2_mask = self.gate_s(input2_mask)
-        input1_att = input1_att * input1_mask
-        input2_att = input2_att * input2_mask
-        input1_att = torch.cat([input1_att, exemplar], 1)
-        input2_att = torch.cat([input2_att, query], 1)
-        input1_att = self.conv1(input1_att)
-        input2_att = self.conv2(input2_att)
-        input1_att = self.bn1(input1_att)
-        input2_att = self.bn2(input2_att)
-        input1_att = self.prelu(input1_att)
-        input2_att = self.prelu(input2_att)
-        x1 = self.main_classifier1(input1_att)
-        x2 = self.main_classifier2(input2_att)
-        x1 = F.upsample(x1, input_size, mode='bilinear')  # upsample to the size of input image, scale=8
-        x2 = F.upsample(x2, input_size, mode='bilinear')  # upsample to the size of input image, scale=8
-        # print("after upsample, tensor size:", x.size())
-        x1 = self.softmax(x1)
-        x2 = self.softmax(x2)
-
-        #        x1 = self.softmax(x1)
-        #        x2 = self.softmax(x2)
-        return x1, x2, temp  # shape: NxCx
-
-
 class FoveaNet(nn.Module):
     def __init__(self, cfg, **kwargs):
         super(FoveaNet, self).__init__()
@@ -769,6 +689,7 @@ class FoveaNet(nn.Module):
         self.roi_num = cfg.MODEL.ROI_NUM
         self.hrnet_only = cfg.MODEL.HRNET_ONLY
         self.hrnet_type = cfg.MODEL.HRNET_TYPE
+        self.TRAIN_AGE_MODEL = cfg.TRAIN.AGE_DATA
         self.feature_ch = 16
         # TODO later
         self.debug_iteration = 0
@@ -853,7 +774,10 @@ class FoveaNet(nn.Module):
 
         self.isAtten = cfg.MODEL.SELF_ATTEN
         self.coAtten = cfg.MODEL.CO_ATTEN
-        self.simpleCA = False  # Note: Fovea by default setting
+        if self.TRAIN_AGE_MODEL:
+            self.simpleCA = True
+        else:
+            self.simpleCA = False  # Note: Fovea by default setting
         self.coAtten_scale = 4.0
         self.add_heatmap_channel = False
         self.image_channel = 3
